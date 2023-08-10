2f5df8cdb646e17be7b99b12e0904497ffa26e55
Kais Chrif <kais.chrif.jobs@gmail.com>
2023-08-10 18:28:38 +0100

    Add cash payment button to border ordering

--- resources/views/clients/complete_order.blade.php
+++ resources/views/clients/complete_order.blade.php
@@ -42,7 +42,7 @@
     </div>
     <div class="form-group{{ $errors->has('comment') ? ' has-danger' : '' }}">
         <label class="form-control-label" for="comment">{{ __('Comment') }}</label>
-        <textarea name="comment" id="comment" class="form-control{{ $errors->has('comment') ? ' is-invalid' : '' }}" placeholder="{{ __( 'Your comment here' ) }} ..." required></textarea>
+        <textarea name="comment" id="comment" class="form-control{{ $errors->has('comment') ? ' is-invalid' : '' }} comment-field" placeholder="{{ __( 'Your comment here' ) }} ..." required></textarea>
         @if ($errors->has('comment'))
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $errors->first('comment') }}</strong>
