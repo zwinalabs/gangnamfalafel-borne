f8896237c99014a537b2a003cbbd070a7433d1ef
Kais Chrif <kais.chrif.jobs@gmail.com>
2023-08-27 01:57:08 +0100

    Add hiboutik api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\Hiboutik\HiboutikController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+/**
+ * Hiboutik Gateway API request
+ */
+Route::group(['middleware' => ['api']], function(){
+    Route::get('/new-sale-hiboutik', 'API\Hiboutik\HiboutikController@newSale')->name('hiboutik.newSale');
+    Route::get('/close-sale-hiboutik', 'API\Hiboutik\HiboutikController@closeSale')->name('hiboutik.closeSale');
+    Route::get('/add-product-sale-hiboutik', 'API\Hiboutik\HiboutikController@addProductToSale')->name('hiboutik.addProductToSale');
+    Route::get('/print-receipt-hiboutik', 'API\Hiboutik\HiboutikController@printReceipt')->name('hiboutik.printReceipt');
+    Route::get('/print-receipt-kitchen-hiboutik', 'API\Hiboutik\HiboutikController@printReceiptKitchen')->name('hiboutik.printReceiptKitchen');
+    Route::get('/print-receipt-order-hiboutik', 'API\Hiboutik\HiboutikController@printOrderHiboutik')->name('hiboutik.printOrderHiboutik');
 });
